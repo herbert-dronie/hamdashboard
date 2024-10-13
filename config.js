f153f205a3a352fbe0b317c8c818b8cdb51d68cb
herbert-dronie <57329832+herbert-dronie@users.noreply.github.com>
2024-10-13 13:23:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ const aURL = [
 // You can't add more items because there are only 12 placeholders on the dashboard
 // but you can replace the titles and the images with anything you want.
 const aIMG = [
-  ["RADAR", "https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=8&overlay=wind&product=ecmwf&level=surface&lat=37.853&lon=-1.088",
+  ["RADAR", "<iframe width="600" height="400" src="https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=mm&metricTemp=°C&metricWind=km/h&zoom=8&overlay=wind&product=ecmwf&level=surface&lat=37.884&lon=-1.225&pressure=true&message=true" frameborder="0"></iframe>",
   ],
   [
     "LOCAL RADAR",
